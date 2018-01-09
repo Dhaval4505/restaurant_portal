a959247ccba5bfa4876e822984f60026859ac774
Dhaval Trivedi <dhaval4505@gmail.com>
2018-01-09 16:49:45 +0530

    User roles defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  # Instance Methods
+  def is_admin?
+  	type == "Admin"
+  end
+
+  def is_customer?
+  	type == "Customer"
+  end
 end
